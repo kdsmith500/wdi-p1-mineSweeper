dae72e99ccf4f16044f1da108f41e3ce48e4c2ce
Kyle Smith <k.d.smith@gmail.com>
2019-02-05 16:27:05 -0600

    adds init function createBoard, play button for draw game board

--- js/main.js
+++ js/main.js
@@ -1,25 +1,24 @@
 /*----- constants -----*/
 
-const width = document.getElementById('width');
-const height = document.getElementById('height');
-const mines = document.getElementById('mines');
 
 /*----- app's state (variables) -----*/
 
-let gameBoard = [];
-
+let w;
+let h;
+let board;
 /*----- cached element references -----*/
 
-let boardWidth = document.getElementById('width').getAttribute('value');
-let boardHeight = document.getElementById('height').getAttribute('value');
-let boardMines = document.getElementById('mines').getAttribute('value');
-
+let boardWidth = document.getElementById('width');
+let boardHeight = document.getElementById('height');
+let boardMines = document.getElementById('mines');
+let titlePage = document.getElementById('title-page');
+let main = document.querySelector('main');
 /*----- event listeners -----*/
 
 document.getElementById('beginner').addEventListener('click', setBeginner);
 document.getElementById('intermediate').addEventListener('click', setIntermediate);
 document.getElementById('expert').addEventListener('click', setExpert);
-// document.getElementById('play').addEventListener('click', createBoard);
+document.getElementById('play').addEventListener('click', createBoard);
 // document.querySelectorAll('.replay')addEventListener('click', MAKE_REPLAY_FUNCTION);
 
 /*----- functions -----*/
@@ -45,19 +44,50 @@ function setExpert() {
     console.log('expert');
 };
 
-// function createBoard() {
-//     let w, h, boardCell;
-//     for (w = 0; w < boardWidth; w++) {
-//         gameBoard[w] = [];
-//         for (h = 0; h < boardHeight; h++) {
-//             boardCell = document.createElement('div');
-//             let boardCellClass = document.createAttribute('class');
-//             boardCellClass.value = "hidden-cell";
-//             boardCell.setAttributeNode(boardCellClass);
-//             boardCell.appendChild();
-//             gameBoard[w][h] = gameBoard(boardCell, w, h);
-//             boardCell.data('location', {x: w, y: h});
-//         }
-//     document.innerHTML('<div class="clear-cell"></div>').appendTo(main);
-//     }
-// }
+function check(x1, y1) {
+    if ((x1 >= 0) && (y1 >= 0) && (x1 <= w) && (y1 <= h))
+        return board[x1+y1*w];
+}
+
+function createBoard() {
+    let mines = boardMines.value;
+    w = boardWidth.value;
+    h = boardHeight.value;
+    let remaining = mines;
+    let cell = [];
+    board = [];
+    let revealed = [];
+    titlePage.style = "display: none";
+    main.style = `display: flex; width: ${w*34}px`;
+    for (let i = 0; i < w*h; i++) {
+        cell[i] = document.createElement('img');
+        cell[i].src = "images/generic-blast.png";
+        cell[i].style = "height: 30px; width: 30px";
+        // cell[i].addEventListener('click', click);
+        cell[i].id = i;
+        main.appendChild(cell[i]);
+    }
+    let placed = 0;
+    do {
+        i = Math.floor(Math.random()*w*h);
+        if (board[i] != 'mine') {
+            board[i] = 'mine';
+            placed++;
+        }
+    } while (placed < mines);
+    for (let x = 0; x < w; x++)
+        for (let y = 0; y < h+1; y++) {
+            if (check(x, y) != 'mine') {
+                board[x+y*w] =
+                ((check(x, y+1) == 'mine') | 0)
+                + ((check(x-1, y+1) == 'mine') | 0)
+                + ((check(x+1, y+1) == 'mine') | 0)
+                + ((check(x, y-1) == 'mine') | 0)
+                + ((check(x-1, y-1) == 'mine') | 0)
+                + ((check(x+1, y-1) == 'mine') | 0)
+                + ((check(x-1, y) == 'mine') | 0)
+                + ((check(x+1, y) == 'mine') | 0);
+            }
+        }
+}
+
