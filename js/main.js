fe2d83b058d3306a1dd8995cd5994326fbc163f1
Kyle Smith <k.d.smith@gmail.com>
2019-02-07 17:53:36 -0600

    working mvp

--- js/main.js
+++ js/main.js
@@ -27,7 +27,7 @@ document.getElementById('beginner').addEventListener('click', setBeginner);
 document.getElementById('intermediate').addEventListener('click', setIntermediate);
 document.getElementById('expert').addEventListener('click', setExpert);
 document.getElementById('play').addEventListener('click', createBoard);
-replay.forEach(function(replayHandler) {
+replay.forEach(function (replayHandler) {
     replayHandler.addEventListener('click', replayButton);
 });
 
@@ -36,19 +36,19 @@ replay.forEach(function(replayHandler) {
 function setBeginner() {
     width.value = '9';
     height.value = '9';
-    mines.value = '10'; 
+    boardMines.value = '10';
 };
 
 function setIntermediate() {
     width.value = '16';
     height.value = '16';
-    mines.value = '40';
+    boardMines.value = '40';
 };
 
 function setExpert() {
     width.value = '16';
     height.value = '30';
-    mines.value = '99';
+    boardMines.value = '99';
 };
 
 function replayButton() {
@@ -58,18 +58,21 @@ function replayButton() {
 }
 
 function loseSequence() {
-    setTimeout(function() {
-    main.style = 'display: none;';
-    loseCondition.style = 'display: grid;';
-    }, 10000);
+    setTimeout(function () {
+        main.style = 'display: none;';
+        loseCondition.style = 'display: grid;';
+    }, 5000);
 }
 
 function check(x1, y1) {
     if ((x1 >= 0) && (y1 >= 0) && (x1 <= w) && (y1 <= h))
-        return board[x1+y1*w];
+        return board[x1 + y1 * w];
 }
 
 function picture(index) {
+    // console.log(cell, '1');
+    // console.log(cell[index], '2');
+    // console.log(cell[index].src, '3');
     return cell[index].src.substr(cell[index].src.length - 5, 1);
 }
 
@@ -83,8 +86,8 @@ function createBoard() {
     revealed = [];
     placed = 0;
     titlePage.style = "display: none;";
-    main.style = `display: flex; width: ${w*34}px;`;
-    for (let i = 0; i < w*h; i++) {
+    main.style = `display: flex; width: ${w * 34}px;`;
+    for (let i = 0; i < w * h; i++) {
         cell[i] = document.createElement('img');
         cell[i].src = "images/x.png";
         cell[i].style = "height: 30px; width: 30px";
@@ -93,24 +96,24 @@ function createBoard() {
         main.appendChild(cell[i]);
     }
     do {
-        i = Math.floor(Math.random()*w*h);
+        i = Math.floor(Math.random() * w * h);
         if (board[i] != 'mine') {
             board[i] = 'mine';
             placed++;
         }
     } while (placed < mines);
     for (let x = 0; x < w; x++)
-        for (let y = 0; y < h+1; y++) {
+        for (let y = 0; y < h + 1; y++) {
             if (check(x, y) != 'mine') {
-                board[x+y*w] =
-                ((check(x, y+1) == 'mine') | 0)
-                + ((check(x-1, y+1) == 'mine') | 0)
-                + ((check(x+1, y+1) == 'mine') | 0)
-                + ((check(x, y-1) == 'mine') | 0)
-                + ((check(x-1, y-1) == 'mine') | 0)
-                + ((check(x+1, y-1) == 'mine') | 0)
-                + ((check(x-1, y) == 'mine') | 0)
-                + ((check(x+1, y) == 'mine') | 0);
+                board[x + y * w] =
+                    ((check(x, y + 1) == 'mine') | 0)
+                    + ((check(x - 1, y + 1) == 'mine') | 0)
+                    + ((check(x + 1, y + 1) == 'mine') | 0)
+                    + ((check(x, y - 1) == 'mine') | 0)
+                    + ((check(x - 1, y - 1) == 'mine') | 0)
+                    + ((check(x + 1, y - 1) == 'mine') | 0)
+                    + ((check(x - 1, y) == 'mine') | 0)
+                    + ((check(x + 1, y) == 'mine') | 0);
             }
         }
 }
@@ -122,17 +125,17 @@ function click(event) {
     if (event.button === 2) {
         switch (picture(id)) {
             case 'x':
-            console.log('flag');
+                console.log('flag');
                 cell[id].src = 'images/f.png';
                 remaining--;
                 break;
             case 'f':
-            console.log('question mark');
+                console.log('question mark');
                 cell[id].src = 'images/q.png';
                 remaining++;
                 break;
             case 'q':
-            console.log('covered');
+                console.log('covered');
                 cell[id].src = 'images/x.png';
                 break;
         }
@@ -143,7 +146,7 @@ function click(event) {
         if (board[id] == 'mine') {
             console.log('mine');
             loseSequence();
-            for (i = 0; i < w*h; i++) {
+            for (i = 0; i < w * h; i++) {
                 if (board[i] == 'mine') {
                     cell[i].src = 'images/m.png';
                 } else if (board[i] != 'mine' && picture(i) == 'f') {
@@ -153,21 +156,45 @@ function click(event) {
         } else if (picture(id) == 'x') {
             console.log(`reveal ${id}`);
             reveal(id);
-        } 
+        }
     }
-    if (revealed == w*h - mines) {
+    if (revealed == w * h - mines) {
         main.style = 'display: none;';
         winCondition.style = 'display: grid;';
     }
 }
 
 function reveal(index) {
+    // debugger
     if (board[index] != 'mine' && picture(index) == 'x') {
         revealed++;
         cell[index].src = 'images/' + board[index] + '.png';
+        if (board[index] == 0) revealAdjacent(index);
     }
-    x = index % w;
-    y = Math.floor(index / w);
+}
+
+function revealAdjacent(index) {
+    index = parseInt(index);
+    w = parseInt(w);
+    h = parseInt(h);
+    let x = index % w;
+    let y = Math.floor(index / w);
+    for (let col = -1; col <= 1; col++) {
+        for (let row = -1; row <= 1; row++) {
+            if ((col === 0 && row === 0)
+                || (col === -1 && x === 0)
+                || (col === 1 && x === w - 1)
+                || (row === -1 && y === 0)
+                || (row === 1 && y === h - 1)
+            ) {
+            } else {
+                let idx = (y + row) * w + (x + col);
+                console.log(col, row, x, y, w, h, idx);
+                if (picture(idx) == 'x') reveal(idx);
+            }
+        }
+    }
+}
     // if (board[index] == 0) {
     //     console.log('you clicked a 0');
     //     switch () {
@@ -196,40 +223,7 @@ function reveal(index) {
     //             console.log('cell up-right');
     //             reveal (index - w + 1);
     //     }
-    if (board[index] == 0) {
-        console.log('you clicked a 0');
-        if (x > 0 && picture(index - 1) == 'x') {
-            console.log('cell left');
-            reveal(index - 1);
-        }
-        if (x < (w - 1) && picture(index + 1) == 'x') {
-            console.log('cell right');
-            reveal(index + 1);
-        }
-        if (y < (h - 1) && picture(index + w) == 'x') {
-            console.log('cell down');
-            reveal(index + w);
-        }
-        if (y > 0 && picture(index - w) == 'x') {
-            console.log('cell up');
-            reveal(index - w);
-        }
-        if (x > 0 && y > 0 && picture(index - w - 1) == 'x') {
-            console.log('cell up-left');
-            reveal(index - w - 1);
-        }                                                   
-        if (x < (w - 1) && y < (h - 1) && picture(index + w + 1) == 'x') {
-            console.log('cell down-right');
-            reveal(index + w + 1);
-        }
-        if (x > 0 && y < (h - 1) && y < (h - 1) && picture(index + w - 1) == 'x') {
-            console.log('cell down-left');
-            reveal(index + w - 1);
-        }
-        if (x < (w - 1) && y > 0 && y < (h - 1) && picture(index - w + 1) == 'x') {
-            console.log('cell up-right');
-            reveal (index - w + 1);
-        }
+
     // if (board[index] == 0) {
     //     console.log('you clicked a 0');
     //     if (x > 0) {
@@ -264,5 +258,39 @@ function reveal(index) {
     //         console.log('cell up-right');
     //         reveal (index - w + 1);
     //     }
-    }
-}
+//>>>>>>original if statements<<<<<<<//
+    // if (board[index] == 0) {
+    //     console.log('you clicked a 0');
+    //     if (x > 0 && picture(index - 1) == 'x') {
+    //         console.log('cell left');
+    //         reveal(index - 1);
+    //     }
+    //     if (x < (w - 1) && picture(index + 1) == 'x') {
+    //         console.log('cell right');
+    //         reveal(index + 1);
+    //     }
+    //     if (y < (h - 1) && picture(index + w) == 'x') {
+    //         console.log('cell down');
+    //         reveal(index + w);
+    //     }
+    //     if (y > 0 && picture(index - w) == 'x') {
+    //         console.log('cell up');
+    //         reveal(index - w);
+    //     }
+    //     if (x > 0 && y > 0 && picture(index - w - 1) == 'x') {
+    //         console.log('cell up-left');
+    //         reveal(index - w - 1);
+    //     }                                                   
+    //     if (x < (w - 1) && y < (h - 1) && picture(index + w + 1) == 'x') {
+    //         console.log('cell down-right');
+    //         reveal(index + w + 1);
+    //     }
+    //     if (x > 0 && y < (h - 1) && y < (h - 1) && picture(index + w - 1) == 'x') {
+    //         console.log('cell down-left');
+    //         reveal(index + w - 1);
+    //     }
+    //     if (x < (w - 1) && y > 0 && y < (h - 1) && picture(index - w + 1) == 'x') {
+    //         console.log('cell up-right');
+    //         reveal (index - w + 1);
+    //     }
+    // }
