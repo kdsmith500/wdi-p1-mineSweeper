2e1c61a449d61ff885da6c9258a501ec894e2a72
Kyle Smith <k.d.smith@gmail.com>
2019-02-08 11:23:58 -0600

    adds styling changes to title page and end screens, add alert to prevent impossible grid/mine combos

--- js/main.js
+++ js/main.js
@@ -1,6 +1,3 @@
-/*----- constants -----*/
-
-
 /*----- app's state (variables) -----*/
 
 let w;
@@ -33,61 +30,64 @@ replay.forEach(function (replayHandler) {
 
 /*----- functions -----*/
 
-function setBeginner() {
+function setBeginner() { /*>>>event handler to set preset beginner board<<<*/
     width.value = '9';
     height.value = '9';
     boardMines.value = '10';
 };
 
-function setIntermediate() {
+function setIntermediate() { /*>>>event handler to set preset intermediate board<<<*/
     width.value = '16';
     height.value = '16';
     boardMines.value = '40';
 };
 
-function setExpert() {
+function setExpert() { /*>>>event handler to set preset expert board<<<*/
     width.value = '16';
     height.value = '30';
     boardMines.value = '99';
 };
 
-function replayButton() {
+function replayButton() { /*>>>replay button on win/lose condition screens<<<*/
     winCondition.style = 'display: none;';
     loseCondition.style = 'display: none;';
     titlePage.style = 'display: grid;';
-    for (let i = 0; i < w*h; i++) {
+    for (let i = 0; i < w*h; i++) { /*>>>this iterates through all appended images in the main that make up the previous game board and deletes them<<<*/
         main.removeChild(main.childNodes[0]);
     }
 }
 
-function loseSequence() {
+function loseSequence() { /*>>>lose sequence, waits 5 seconds so player can see the board and then switches screens<<<*/
     setTimeout(function () {
         main.style = 'display: none;';
         loseCondition.style = 'display: grid;';
     }, 5000);
 }
 
-function check(x1, y1) {
+function check(x1, y1) { /*>>>checks if the cell index it is called on exists in the game board, if it is it returns the value of the index in the board<<<*/
     if ((x1 >= 0) && (y1 >= 0) && (x1 <= w) && (y1 <= h))
         return board[x1 + y1 * w];
 }
 
-function picture(index) {
+function picture(index) { /*>>>this function takes the image src in the cell index, counts back 5 in the src name and returns 1 string, so its returns the file name corresponding to 0-8, mine, flag, ?, cake<<<*/
     return cell[index].src.substr(cell[index].src.length - 5, 1);
 }
 
-function createBoard() {
+function createBoard() { /*>>>this in the initialization function that creates the game board<<<*/
     mines = boardMines.value;
     w = boardWidth.value;
     h = boardHeight.value;
+    if (mines > w * h) {
+        return alert("You must define a board with enough room for your mines!");
+    }
     remaining = mines;
-    cell = [];
-    board = [];
-    revealed = [];
-    placed = 0;
+    cell = []; /*>>>images are stored in here, index represents cell in area of board<<<*/
+    board = []; /*>>>value representing whats hidden in the cell<<<*/
+    revealed = []; /*>>>count of cells revealed by player<<<*/
+    placed = 0; /*>>>count of mines placed on generation of board<<<*/
     titlePage.style = "display: none;";
     main.style = `display: flex; width: ${w * 34}px;`;
-    for (let i = 0; i < w * h; i++) {
+    for (let i = 0; i < w * h; i++) { /*>>>this loops through the area of the board to create images representing each cell<<<*/
         cell[i] = document.createElement('img');
         cell[i].src = "images/x.png";
         cell[i].style = "height: 30px; width: 30px";
@@ -96,15 +96,15 @@ function createBoard() {
         main.appendChild(cell[i]);
     }
     do {
-        i = Math.floor(Math.random() * w * h);
+        i = Math.floor(Math.random() * w * h); /*>>>this creates a random number inside the specified board area and asigns a mine until placed = mines<<<*/
         if (board[i] != 'mine') {
             board[i] = 'mine';
             placed++;
         }
-    } while (placed < mines);
+    } while (placed < mines); /*>>>this is a nested for loop that runs as mines are placed and will iterate through every cell in the board<<<*/
     for (let x = 0; x < w; x++)
         for (let y = 0; y < h + 1; y++) {
-            if (check(x, y) != 'mine') {
+            if (check(x, y) != 'mine') { /*>>>each mine it encounters will cause it to call the check function on every adjacent cell and decide either 1 or 0 and sum the total, that total then becomes what occupies that cell<<<*/
                 board[x + y * w] =
                     ((check(x, y + 1) == 'mine') | 0)
                     + ((check(x - 1, y + 1) == 'mine') | 0)
@@ -118,10 +118,10 @@ function createBoard() {
         }
 }
 
-function click(event) {
+function click(event) { /*>>>this event listens for the mouse click on the particular cell clicked<<<*/
     let source = event.target;
     let id = source.id;
-    if (event.button === 2) {
+    if (event.button === 2) { /*>>>this switch is entered if the mouse event is a right click and cycles through flag, question mark, back to covered cell<<<*/
         switch (picture(id)) {
             case 'x':
                 cell[id].src = 'images/f.png';
@@ -136,8 +136,8 @@ function click(event) {
                 break;
         }
     }
-    if (event.button === 0 && picture(id) != 'f' && picture(id) != 'q') {
-        if (board[id] == 'mine') {
+    if (event.button === 0 && picture(id) != 'f' && picture(id) != 'q') { /*>>>if the event is a left click and not on a flag or question mark<<<*/
+        if (board[id] == 'mine') { /*>>>if you left click a mine, lose sequence is initiated and a for loop iterates through the entire board revealing all mines and incorrectly palces flags<<<*/
             loseSequence();
             for (i = 0; i < w * h; i++) {
                 if (board[i] == 'mine') {
@@ -146,39 +146,39 @@ function click(event) {
                     cell[i].src = 'images/e.png';
                 }
             }
-        } else if (picture(id) == 'x') {
+        } else if (picture(id) == 'x') { /*>>>otherwise pass the index clicked in the reveal function<<<*/
             reveal(id);
         }
     }
-    if (revealed == w * h - mines) {
+    if (revealed == w * h - mines) { /*>>>if the revealed cell count equals the board area minus the mines, win sequence is initiated<<<*/
         main.style = 'display: none;';
         winCondition.style = 'display: grid;';
     }
 }
 
-function reveal(index) {
+function reveal(index) { /*>>>this checks if the cell is covered and not a mine<<<*/
     if (board[index] != 'mine' && picture(index) == 'x') {
         revealed++;
-        cell[index].src = 'images/' + board[index] + '.png';
+        cell[index].src = 'images/' + board[index] + '.png'; /*>>>this takes the string in the cell that is to be revealed and concatenates it into the file path for the image to be revealed<<<*/
         if (board[index] == 0) revealAdjacent(index);
     }
 }
 
-function revealAdjacent(index) {
-    index = parseInt(index);
+function revealAdjacent(index) { /*>>>after the cell interacted with is revealed the index is then passed into this function<<<*/
+    index = parseInt(index); /*>>>these make sure the grid width, height, and the index selected are all numbers<<<*/
     w = parseInt(w);
     h = parseInt(h);
-    let x = index % w;
+    let x = index % w; /*>>>these functions will turn any cell index into an x, y coorinate representing its position counting from 0<<<*/
     let y = Math.floor(index / w);
-    for (let col = -1; col <= 1; col++) {
+    for (let col = -1; col <= 1; col++) { /*>>>this is a nexted for loop that will iterate through all cells adjacent to the cell that triggered the initial event<<<*/
         for (let row = -1; row <= 1; row++) {
-            if ((col === 0 && row === 0)
+            if ((col === 0 && row === 0) /*>>>if any of the postions exits outside of the board, return nothing<<<*/
                 || (col === -1 && x === 0)
                 || (col === 1 && x === w - 1)
                 || (row === -1 && y === 0)
                 || (row === 1 && y === h - 1)
             ) {
-            } else {
+            } else { /*>>>otherwise pass each unrevealed existing adjacent cell index back into the reveal function<<<*/
                 let idx = (y + row) * w + (x + col);
                 if (picture(idx) == 'x') reveal(idx);
             }
